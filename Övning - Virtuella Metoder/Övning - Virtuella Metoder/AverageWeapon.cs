e5f950eebd7706a7a87d9bebaa5a1add13e51754
GUSTOfWinds <gustavkjonsson01@gmail.com>
2019-10-17 16:02:05 +0200

    Summary

---
+++ Övning - Virtuella Metoder/Övning - Virtuella Metoder/AverageWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning___Virtuella_Metoder
+{
+    class AverageWeapon: Weapon
+    {
+        public override int Attack()
+        {
+            int avgDamage = (minDamage + maxDamage) / 2;
+            return avgDamage;
+        }
+    }
+}
---
+++ Övning - Virtuella Metoder/Övning - Virtuella Metoder/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning___Virtuella_Metoder
+{
+    class Weapon
+    {
+        protected int minDamage = 1;
+        protected int maxDamage = 10;
+
+        protected Random randomgenerator = new Random();
+
+        public virtual int Attack()
+        {
+            return randomgenerator.Next(minDamage, maxDamage);
+        }
+    }
+}
